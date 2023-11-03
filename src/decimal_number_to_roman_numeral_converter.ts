3df98020c843abfdc33443f6869e50ad795ad9c6
Shahnaaz Rahamatullah <rahamsha@gmail.com>
2023-11-03 15:42:29 +0000

    feat: add logic and test for converting remaining decimal numbers to roman numerals

--- src/decimal_number_to_roman_numeral_converter.ts
+++ src/decimal_number_to_roman_numeral_converter.ts
@@ -4,6 +4,51 @@ export const decimalNumberToRomanNumeralConverter = (
 ): string => {
   let romanNumeral = "";
 
+  while (decimalNumber >= 1000) {
+    romanNumeral = `${romanNumeral}M`;
+    decimalNumber = decimalNumber - 1000;
+  }
+
+  while (decimalNumber >= 900) {
+    romanNumeral = `${romanNumeral}CM`;
+    decimalNumber = decimalNumber - 900;
+  }
+
+  while (decimalNumber >= 500) {
+    romanNumeral = `${romanNumeral}D`;
+    decimalNumber = decimalNumber - 500;
+  }
+
+  while (decimalNumber >= 400) {
+    romanNumeral = `${romanNumeral}CD`;
+    decimalNumber = decimalNumber - 400;
+  }
+
+  while (decimalNumber >= 100) {
+    romanNumeral = `${romanNumeral}C`;
+    decimalNumber = decimalNumber - 100;
+  }
+
+  while (decimalNumber >= 90) {
+    romanNumeral = `${romanNumeral}XC`;
+    decimalNumber = decimalNumber - 90;
+  }
+
+  while (decimalNumber >= 50) {
+    romanNumeral = `${romanNumeral}L`;
+    decimalNumber = decimalNumber - 50;
+  }
+
+  while (decimalNumber >= 40) {
+    romanNumeral = `${romanNumeral}XL`;
+    decimalNumber = decimalNumber - 40;
+  }
+
+  while (decimalNumber >= 10) {
+    romanNumeral = `${romanNumeral}X`;
+    decimalNumber = decimalNumber - 10;
+  }
+
   while (decimalNumber >= 9) {
     romanNumeral = `${romanNumeral}IX`;
     decimalNumber = decimalNumber - 9;
--- tests/decimal_number_to_roman_numeral_converter.test.ts
+++ tests/decimal_number_to_roman_numeral_converter.test.ts
@@ -21,11 +21,83 @@ describe("test decimalNumberToRomanNumeralConverter function", () => {
     expect(decimalNumberToRomanNumeralConverter(5)).toBe("V");
   });
 
-  it("should return V for decimalNumberToRomanNumeralConverter(7)", () => {
+  it("should return VII for decimalNumberToRomanNumeralConverter(7)", () => {
     expect(decimalNumberToRomanNumeralConverter(7)).toBe("VII");
   });
 
-  it("should return V for decimalNumberToRomanNumeralConverter(9)", () => {
+  it("should return IX for decimalNumberToRomanNumeralConverter(9)", () => {
     expect(decimalNumberToRomanNumeralConverter(9)).toBe("IX");
+  });
+
+  it("should return X for decimalNumberToRomanNumeralConverter(10)", () => {
+    expect(decimalNumberToRomanNumeralConverter(10)).toBe("X");
+  });
+
+  it("should return XXXIX for decimalNumberToRomanNumeralConverter(39)", () => {
+    expect(decimalNumberToRomanNumeralConverter(39)).toBe("XXXIX");
+  });
+
+  it("should return XL for decimalNumberToRomanNumeralConverter(40)", () => {
+    expect(decimalNumberToRomanNumeralConverter(40)).toBe("XL");
+  });
+
+  it("should return XLVI for decimalNumberToRomanNumeralConverter(46)", () => {
+    expect(decimalNumberToRomanNumeralConverter(46)).toBe("XLVI");
+  });
+
+  it("should return L for decimalNumberToRomanNumeralConverter(50)", () => {
+    expect(decimalNumberToRomanNumeralConverter(50)).toBe("L");
+  });
+
+  it("should return LXXVII for decimalNumberToRomanNumeralConverter(77)", () => {
+    expect(decimalNumberToRomanNumeralConverter(77)).toBe("LXXVII");
+  });
+
+  it("should return XC for decimalNumberToRomanNumeralConverter(90)", () => {
+    expect(decimalNumberToRomanNumeralConverter(90)).toBe("XC");
+  });
+
+  it("should return XCIX for decimalNumberToRomanNumeralConverter(99)", () => {
+    expect(decimalNumberToRomanNumeralConverter(99)).toBe("XCIX");
+  });
+
+  it("should return C for decimalNumberToRomanNumeralConverter(100)", () => {
+    expect(decimalNumberToRomanNumeralConverter(100)).toBe("C");
+  });
+
+  it("should return CCCXLVIII for decimalNumberToRomanNumeralConverter(348)", () => {
+    expect(decimalNumberToRomanNumeralConverter(348)).toBe("CCCXLVIII");
+  });
+
+  it("should return CD for decimalNumberToRomanNumeralConverter(400)", () => {
+    expect(decimalNumberToRomanNumeralConverter(400)).toBe("CD");
+  });
+
+  it("should return CDXIII for decimalNumberToRomanNumeralConverter(413)", () => {
+    expect(decimalNumberToRomanNumeralConverter(413)).toBe("CDXIII");
+  });
+
+  it("should return D for decimalNumberToRomanNumeralConverter(500)", () => {
+    expect(decimalNumberToRomanNumeralConverter(500)).toBe("D");
+  });
+
+  it("should return DCXCIV for decimalNumberToRomanNumeralConverter(694)", () => {
+    expect(decimalNumberToRomanNumeralConverter(694)).toBe("DCXCIV");
+  });
+
+  it("should return CM for decimalNumberToRomanNumeralConverter(900)", () => {
+    expect(decimalNumberToRomanNumeralConverter(900)).toBe("CM");
+  });
+
+  it("should return CMXXI for decimalNumberToRomanNumeralConverter(921)", () => {
+    expect(decimalNumberToRomanNumeralConverter(921)).toBe("CMXXI");
+  });
+
+  it("should return M for decimalNumberToRomanNumeralConverter(1000)", () => {
+    expect(decimalNumberToRomanNumeralConverter(1000)).toBe("M");
+  });
+
+  it("should return MMMX for decimalNumberToRomanNumeralConverter(3010)", () => {
+    expect(decimalNumberToRomanNumeralConverter(3010)).toBe("MMMX");
   });
 });
