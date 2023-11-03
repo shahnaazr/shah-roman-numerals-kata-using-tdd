7f8fc268065e0abe2f7fe336b8564e422a126dd6
Shahnaaz Rahamatullah <shahnaaz.rahamatullah@jugo.io>
2023-11-03 14:27:19 +0000

    feat: add logic and test for converting decimal number 2 to roman numeral

--- src/decimal_number_to_roman_numeral_converter.ts
+++ src/decimal_number_to_roman_numeral_converter.ts
@@ -2,6 +2,9 @@ export const decimalNumberToRomanNumeralConverter = (
   decimalNumber: number
 ): string => {
   const romanNumeral = "";
+  if (decimalNumber === 2) {
+    return "II";
+  }
   if (decimalNumber === 1) {
     return "I";
   }
--- tests/decimal_number_to_roman_numeral_converter.test.ts
+++ tests/decimal_number_to_roman_numeral_converter.test.ts
@@ -4,4 +4,8 @@ describe("test decimalNumberToRomanNumeralConverter function", () => {
   it("should return I for decimalNumberToRomanNumeralConverter(1)", () => {
     expect(decimalNumberToRomanNumeralConverter(1)).toBe("I");
   });
+
+  it("should return II for decimalNumberToRomanNumeralConverter(2)", () => {
+    expect(decimalNumberToRomanNumeralConverter(2)).toBe("II");
+  });
 });
